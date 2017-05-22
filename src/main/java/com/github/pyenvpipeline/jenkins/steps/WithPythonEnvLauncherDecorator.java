5d7d94c51a83b55976297992b0f8ecdcab72ca7d
Colin Starner <colin.starner@gmail.com>
2017-05-22 16:02:35 -0600

    . WIP

---
+++ src/main/java/com/github/pyenvpipeline/jenkins/steps/WithPythonEnvLauncherDecorator.java
@@ -0,0 +1,57 @@
+package com.github.pyenvpipeline.jenkins.steps;
+
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.LauncherDecorator;
+import hudson.Proc;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.remoting.Channel;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+
+public class WithPythonEnvLauncherDecorator extends LauncherDecorator implements Serializable{
+
+    private String commandPrefix;
+    private String pythonVersion;
+
+    public WithPythonEnvLauncherDecorator(String commandPrefix, String pythonVersion){
+        super();
+        this.commandPrefix = commandPrefix;
+        this.pythonVersion = pythonVersion;
+    }
+
+    @Override
+    public Launcher decorate(final Launcher base, Node node) {
+        return new Launcher(base) {
+            @Override
+            public Proc launch(ProcStarter procStarter) throws IOException {
+                TaskListener taskListener = getListener();
+                PrintStream logger = taskListener.getLogger();
+                logger.println("Switching Commands to "+pythonVersion);
+
+                List<String> commands = procStarter.cmds();
+                String firstCommand = commandPrefix + commands.get(0);
+                commands.set(0, firstCommand);
+                procStarter.cmds(commands);
+
+                return base.launch(procStarter);
+            }
+
+            @Override
+            public Channel launchChannel(String[] strings, OutputStream outputStream, FilePath filePath, Map<String, String> map) throws IOException, InterruptedException {
+                return base.launchChannel(strings, outputStream, filePath, map);
+            }
+
+            @Override
+            public void kill(Map<String, String> map) throws IOException, InterruptedException {
+                base.kill(map);
+            }
+        };
+    }
+}
--- src/main/java/com/github/pyenvpipeline/jenkins/steps/WithPythonEnvStep.java
+++ src/main/java/com/github/pyenvpipeline/jenkins/steps/WithPythonEnvStep.java
@@ -1,41 +1,47 @@
 package com.github.pyenvpipeline.jenkins.steps;
+import com.google.common.collect.ImmutableSet;
 import hudson.*;
-import hudson.model.Descriptor;
-import hudson.model.Node;
-import hudson.remoting.Channel;
-import net.sf.json.JSONObject;
+import hudson.model.TaskListener;
+import hudson.util.ArgumentListBuilder;
 import org.jenkinsci.plugins.workflow.steps.*;
-import org.kohsuke.stapler.DataBoundSetter;
-import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.Nonnull;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Collections;
-import java.util.Map;
+import java.io.Serializable;
 import java.util.Set;
 
-public class WithPythonEnvStep extends Step {
+public class WithPythonEnvStep extends Step implements Serializable{
 
-    private String pythonEnvLocation;
-    private static final String DEFAULT_DIR = ".pyenv";
+    private String pythonVersion;
+    private static final String DEFAULT_DIR_PREFIX = ".pyenv";
+
+
+    @DataBoundConstructor
+    public WithPythonEnvStep(String pythonVersion){
+        this.pythonVersion = pythonVersion;
+    }
+
+    public String getPythonVersion() {
+        return pythonVersion;
+    }
+
+    public String getCommandPrefix(StepContext stepContext) throws Exception {
+        return "source "+getFullyQualifiedPythonEnvDirectoryName(stepContext) + "/bin/activate && ";
+    }
+
+    public String getRelativePythonEnvDirectory(){
+        return DEFAULT_DIR_PREFIX +"-"+ pythonVersion.replaceAll("/", "-");
+    }
+
+    public String getFullyQualifiedPythonEnvDirectoryName(StepContext stepContext) throws Exception{
+        EnvVars envVars = stepContext.get(EnvVars.class);
+        String workspace = envVars.get("WORKSPACE");
+        return workspace + "/" + getRelativePythonEnvDirectory();
+    }
 
     @Override
     public StepExecution start(StepContext stepContext) throws Exception {
         return new Execution(this, stepContext);
-    }
-
-    @DataBoundSetter
-    public void setPythonEnvLocation(String pythonEnvLocation){
-        if ("".equals(pythonEnvLocation)) {
-            this.pythonEnvLocation = DEFAULT_DIR;
-        } else {
-            this.pythonEnvLocation = pythonEnvLocation;
-        }
-    }
-
-    public String getPythonEnvLocation(){
-        return this.pythonEnvLocation;
     }
 
 
@@ -48,11 +54,36 @@ public class WithPythonEnvStep extends Step {
             this.step = step;
         }
 
+        private void createPythonEnv(StepContext stepContext) throws Exception{
+            ArgumentListBuilder command = new ArgumentListBuilder();
+
+            command.add("virtualenv");
+            command.add("--python="+step.getPythonVersion());
+            command.add(step.getFullyQualifiedPythonEnvDirectoryName(stepContext));
+
+            Launcher launcher = stepContext.get(Launcher.class);
+            Launcher.ProcStarter procStarter = launcher.launch();
+            procStarter.cmds(command);
+            procStarter.join();
+        }
+
         @Override
         public boolean start() throws Exception {
             StepContext context = getContext();
+
+            // Setup environment properly
+
+            if (!context.get(FilePath.class).child(step.getRelativePythonEnvDirectory()).exists()) {
+                createPythonEnv(context);
+            }
+
+            // Create a merged LauncherDecorator, add it to the BodyInvoker, and run the code block
+
+            WithPythonEnvLauncherDecorator launcherDecorator = new WithPythonEnvLauncherDecorator(step.getCommandPrefix(context), step.getPythonVersion());
+            LauncherDecorator merged = BodyInvoker.mergeLauncherDecorators(context.get(LauncherDecorator.class), launcherDecorator);
+
             context.newBodyInvoker().
-                    withContext(context).
+                    withContext(merged).
                     withCallback(BodyExecutionCallback.wrap(getContext())).
                     start();
             return false;
@@ -79,31 +110,7 @@ public class WithPythonEnvStep extends Step {
 
         @Override
         public Set<? extends Class<?>> getRequiredContext() {
-            return Collections.emptySet();
-        }
-    }
-
-    private class PyenvLauncherDecorate extends LauncherDecorator {
-
-        @Override
-        public Launcher decorate(final Launcher base, Node node) {
-
-            return new Launcher(base) {
-                @Override
-                public Proc launch(ProcStarter procStarter) throws IOException {
-                    return null;
-                }
-
-                @Override
-                public Channel launchChannel(String[] strings, OutputStream outputStream, FilePath filePath, Map<String, String> map) throws IOException, InterruptedException {
-                    return null;
-                }
-
-                @Override
-                public void kill(Map<String, String> map) throws IOException, InterruptedException {
-                    base.kill(map);
-                }
-            };
+            return ImmutableSet.of(TaskListener.class, FilePath.class, Launcher.class, EnvVars.class);
         }
     }
 }
